9a1f2316615acc166df06bb1f2e1c8e5d5779cc3
Junyoung Kim <jun.gamedev@gmail.com>
2021-04-14 00:52:18 +0900

    세팅 변경

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `nearly_0의 TIL`,
+  description: `Blog posted about Game Dev, C++, Graphics, and Game Engines`,
+  author: `nearly_0`,
+  introduction: `회사에서 C++을 사용하고 싶어하지만, Lua로 고통 받고 있는 게임 개발자.`,
+  siteUrl: `https://nearly0.netlify.com`, // Your blog site url
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    twitter: `nearly_0`, // Your Twitter account
+    github: `nearlyZer0.github.com`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
@@ -15,13 +15,13 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'NearlyZer0/personal_blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'NearlyZer0',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
