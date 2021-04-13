fa918cde7397605e388d1516bc80180e63cf818e
Junyoung Kim <jun.gamedev@gmail.com>
2021-04-14 01:33:58 +0900

    Changed copyright

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,7 +4,7 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/JaeYeopHan">Jbee</a>, Built with{' '}
+    ©<a href="https://github.com/nearlyZer0">nearly_0</a>, Built with{' '}
     <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
       Gatsby-starter-bee
     </a>
