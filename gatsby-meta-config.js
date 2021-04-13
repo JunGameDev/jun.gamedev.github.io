a44675914efebc667aace0a61a3ec65745ab93e7
Junyoung Kim <jun.gamedev@gmail.com>
2021-04-14 01:27:53 +0900

    Changed github link

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -2,11 +2,11 @@ module.exports = {
   title: `nearly_0의 TIL`,
   description: `Blog posted about Game Dev, C++, Graphics, and Game Engines`,
   author: `nearly_0`,
-  introduction: `회사에서 C++을 사용하고 싶어하지만, Lua로 고통 받고 있는 게임 개발자.`,
+  introduction: `회사에서 C++을 사용하고 싶어하지만, Lua로 고통 받고 있는 클라이언트 프로그래머.`,
   siteUrl: `https://nearly0.netlify.com`, // Your blog site url
   social: {
     twitter: `nearly_0`, // Your Twitter account
-    github: `nearlyZer0.github.com`, // Your GitHub account
+    github: `nearlyZer0`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
@@ -15,13 +15,13 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'NearlyZer0/personal_blog', // Your repository for archive comment
+    utterances: 'nearlyZer0/personal_blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'NearlyZer0',
+    buyMeACoffeeId: 'nearlyZer0',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
